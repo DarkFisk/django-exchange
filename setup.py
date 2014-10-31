fbd13acb0812fe79ed87a68432f8dd4b01bc306f
vlopatyn <vlopatyn@altertech.net>
2014-10-31 12:25:40 +0200

    added py-moneyed as dependency

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
     description='currency, historical exchange rates and conversions support for django',
     author='DarkFisk',
     author_email='darkfisk@gmail.com',
-    url='https://github.com/DarkFisk/django-exchange'
+    url='https://github.com/DarkFisk/django-exchange',
+    requires=[
+        "py-moneyed>0.4"
+    ]
 )
